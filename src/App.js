b2b123ad6bf8437cdbed68e9bf819e7a91f29953
TamaraHrybulia <tamara.grybulia@gmail.com>
2024-10-14 21:06:45 +0300

    added bootstrap toolkit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import React from "react";
+
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Learn React</div>;
+  return (
+    <div className="App">
+      Learn react
+      <button type="button" className="btn btn-primary">
+        click
+      </button>
+    </div>
+  );
 }
